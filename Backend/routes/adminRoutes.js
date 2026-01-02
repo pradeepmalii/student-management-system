0a3d341ee7e3740ec767373f441ac231627703a8
Pradeep Mali <malipradeep591@gmail.com>
2026-01-03 01:37:17 +0530

    Fix case-sensitive Admin model import

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const Admin = require("../Models/admin");
+const Admin = require("../Models/admin.js");
 
 const router = express.Router();
 
